bb3ff15ef1484c5246a499a297c174d991ea9062
cynthiacheng1 <cc5469@nyu.edu>
2021-12-04 16:18:07 -0500

    removing quasi constant features

--- featureSelection.py
+++ featureSelection.py
@@ -12,8 +12,6 @@ warnings.filterwarnings('ignore')
 
 df = pd.read_csv("mushrooms.csv")
 
-corr_data = df.copy()
-
 #It's not important what character is what number for calculate. Just turn it into digit value.
 def to_digit(i):
     if i in chars:
@@ -22,15 +20,12 @@ def to_digit(i):
         chars[i] = len(chars)+1
         return chars[i]
     
-features = list(corr_data.columns)
+features = list(df.columns)
 
 for idx in features:
     chars = {}
-    corr_data[idx] = corr_data[idx].map(to_digit)
+    df[idx] = df[idx].map(to_digit)
 
-print(corr_data)
-
-df = corr_data
 
 X = df.loc[ : , df.columns != 'class']
 y = np.array(df['class'])
@@ -38,31 +33,32 @@ y = np.array(df['class'])
 print("shape of X: ", X.shape)
 print("shape of y: ", y.shape)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+
+X_train = X[:4874]
+X_test = X[4874:]
 
 print(X_train.shape, X_test.shape)
 
+#removing constant and quasi constant features
 # using sklearn variancethreshold to find constant features
-sel = VarianceThreshold(threshold=0)
+
+sel = VarianceThreshold(threshold=0.01) #threshold = 0 for constant only
 sel.fit(X_train)  # fit finds the features with zero variance
 
 # get_support is a boolean vector that indicates which features are retained
 # if we sum over get_support, we get the number of features that are not constant
-sum(sel.get_support())
+X_train = X[:4874]
 
-# alternate way of finding non-constant features
-len(X_train.columns[sel.get_support()])
+sum(sel.get_support())
+print(len([x for x in X_train.columns if x not in X_train.columns[sel.get_support()]]))
 
 # print the constant features
-print(
-    len([
-        x for x in X_train.columns
-        if x not in X_train.columns[sel.get_support()]
-    ]))
-
-[x for x in X_train.columns if x not in X_train.columns[sel.get_support()]]
+print([x for x in X_train.columns if x not in X_train.columns[sel.get_support()]])
 
 X_train = sel.transform(X_train)
 X_test = sel.transform(X_test)
 
 print( X_train.shape, X_test.shape)
+
+print(X_train)
